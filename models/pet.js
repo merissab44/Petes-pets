3a5343a3e0c8fb8aec8805406083bbb9b9d78975
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-08-30 22:11:46 -0700

    implemented model for pet and added mongoose pagination

--- models/pet.js
+++ models/pet.js
@@ -3,6 +3,14 @@
 const mongoose = require('mongoose'),
         Schema = mongoose.Schema;
 
+//initialize mongoose paginate
+const mongoosePaginate = require('mongoose-paginate');
+
+//set the default options
+mongoosePaginate.paginate.options = {
+  limit: 3 // how many records on each page
+};
+
 const PetSchema = new Schema({
     name            : { type: String, required: true }
   , species         : { type: String }
@@ -16,4 +24,5 @@ const PetSchema = new Schema({
   timestamps: true
 });
 
+PetSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Pet', PetSchema);
