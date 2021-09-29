f6cf229e01d0a18b197f8465b38bc0388147cd22
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-09-28 20:37:00 -0700

    Implemented Text Search

--- models/pet.js
+++ models/pet.js
@@ -26,5 +26,7 @@ const PetSchema = new Schema({
   timestamps: true
 });
 
+PetSchema.index({ name: 'text', species: 'text', favoriteFood: 'text', description: 'text' });
+
 PetSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model('Pet', PetSchema);
