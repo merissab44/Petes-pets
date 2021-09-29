0af23c51b89e2259a9c39d6d951c087e59e538f3
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-09-28 21:11:33 -0700

    Implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -35,6 +35,18 @@ describe('Pets', ()  => {
         });
   });
 
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+        .get('/')
+        .set('content-type', 'application/json')
+        .end(function(err, res){
+          res.should.have.status(200);
+          res.should.be.json;
+          res.body.should.be.a('object');
+          done();
+        });
+  });
+
   // TEST NEW
   it('should display new form on /pets/new GET', (done) => {
     chai.request(server)
